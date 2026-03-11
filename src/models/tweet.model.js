e083b9e82b64ef32cccae54614517fb1c32d9238
mr_vau <mdalif1434@gmail.com>
2026-03-12 00:04:46 +0600

    feat: define schemas for comment, like, playlist, and tweet models

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,5 +1,17 @@
-import mongoose, {Schema} from "mongoose"
+import mongoose, { Schema } from "mongoose";
 
-const tweetSchema = new Schema({}, {timestamps: true})
+const tweetSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export const Tweet = mongoose.model("Tweet", tweetSchema);
